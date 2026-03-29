eb78eaa6ecc74fe08e910bf3b75600e5b700b23e
niku <10890+niku@users.noreply.github.com>
2026-03-29 01:28:22 +0000

    Add numeric cell read/write support

--- test/e2e/writer_interoperability_test.rb
+++ test/e2e/writer_interoperability_test.rb
@@ -36,6 +36,21 @@ class WriterInteroperabilityTest < Test::Unit::TestCase
     File.delete(xlsx_path) if xlsx_path && File.exist?(xlsx_path)
   end
 
+  test "writer output stores numeric cells correctly" do
+    xlsx_tempfile = Tempfile.new(["xlsxrb-writer", ".xlsx"])
+    xlsx_path = xlsx_tempfile.path
+    xlsx_tempfile.close
+
+    writer = Xlsxrb::Writer.new
+    writer.set_cell("A1", 42)
+    writer.set_cell("B1", 3.14)
+    writer.write(xlsx_path)
+
+    assert_openxml_sdk_scenario_passes("writer_numeric_test", xlsx_path)
+  ensure
+    File.delete(xlsx_path) if xlsx_path && File.exist?(xlsx_path)
+  end
+
   private
 
   def assert_openxml_sdk_scenario_passes(scenario_name, xlsx_path)
