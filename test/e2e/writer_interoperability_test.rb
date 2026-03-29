32f7bb71d36c9440f7fec4a8d7583f25a88eb3a9
niku <10890+niku@users.noreply.github.com>
2026-03-29 01:46:48 +0000

    Add column width read/write support

--- test/e2e/writer_interoperability_test.rb
+++ test/e2e/writer_interoperability_test.rb
@@ -98,6 +98,22 @@ class WriterInteroperabilityTest < Test::Unit::TestCase
     File.delete(xlsx_path) if xlsx_path && File.exist?(xlsx_path)
   end
 
+  test "writer output stores column widths correctly" do
+    xlsx_tempfile = Tempfile.new(["xlsxrb-writer", ".xlsx"])
+    xlsx_path = xlsx_tempfile.path
+    xlsx_tempfile.close
+
+    writer = Xlsxrb::Writer.new
+    writer.set_cell("A1", "hello")
+    writer.set_column_width("A", 20.0)
+    writer.set_column_width("C", 15.5)
+    writer.write(xlsx_path)
+
+    assert_openxml_sdk_scenario_passes("writer_column_width_test", xlsx_path)
+  ensure
+    File.delete(xlsx_path) if xlsx_path && File.exist?(xlsx_path)
+  end
+
   private
 
   def assert_openxml_sdk_scenario_passes(scenario_name, xlsx_path)
