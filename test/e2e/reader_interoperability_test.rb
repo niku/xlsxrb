569c7ce4d064251c406942e41f8e8c0980167537
niku <10890+niku@users.noreply.github.com>
2026-03-31 03:58:17 +0000

    Support scatterStyle and radarStyle for scatter and radar charts
    Add scatter_style and radar_style parameters to add_chart. Emit
    <c:scatterStyle> and <c:radarStyle> as first children of their
    respective chart type elements per ECMA-376 CT_ScatterChart and
    CT_RadarChart. Default to lineMarker and standard respectively.
    Parse in ChartTypeListener and expose in chart hash.

--- test/e2e/reader_interoperability_test.rb
+++ test/e2e/reader_interoperability_test.rb
@@ -1959,6 +1959,21 @@ class ReaderInteroperabilityTest < Test::Unit::TestCase
     File.delete(xlsx_path) if xlsx_path && File.exist?(xlsx_path)
   end
 
+  test "reader parses SDK-generated scatterStyle element in scatter chart" do
+    xlsx_tempfile = Tempfile.new(["xlsxrb-reader-e2e", ".xlsx"])
+    xlsx_path = xlsx_tempfile.path
+    xlsx_tempfile.close
+
+    assert_openxml_sdk_scenario_passes("reader_scatter_style_generated_by_sdk", xlsx_path)
+
+    reader = Xlsxrb::Reader.new(xlsx_path)
+    charts = reader.charts
+    assert_equal(1, charts.size)
+    assert_equal("lineMarker", charts[0][:scatter_style])
+  ensure
+    File.delete(xlsx_path) if xlsx_path && File.exist?(xlsx_path)
+  end
+
   private
 
   def assert_openxml_sdk_scenario_passes(scenario_name, xlsx_path)
