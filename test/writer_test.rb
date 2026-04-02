257f3b1f2b1e919af7473310a85f1cbcdcf0e170
niku <10890+niku@users.noreply.github.com>
2026-04-02 00:24:10 +0000

    Support shape text paragraph tab stops

--- test/writer_test.rb
+++ test/writer_test.rb
@@ -5349,6 +5349,23 @@ class WriterTest < Test::Unit::TestCase
     File.delete(xlsx_path) if xlsx_path && File.exist?(xlsx_path)
   end
 
+  test "add_shape with text_tab_stops emits a:tabLst in a:pPr" do
+    writer = Xlsxrb::Writer.new
+    writer.set_cell("A1", 1)
+    writer.add_shape(preset: "rect", text: "Tabs",
+                     text_tab_stops: [{ pos: 914_400, align: "l" }, { pos: 1_828_800, align: "r" }])
+
+    xlsx_tempfile = Tempfile.new(["xlsxrb-tabs", ".xlsx"])
+    xlsx_path = xlsx_tempfile.path
+    xlsx_tempfile.close
+    writer.write(xlsx_path)
+
+    drawing_xml = read_xml_from_xlsx(xlsx_path, "xl/drawings/drawing1.xml")
+    assert_match(%r{<a:tabLst><a:tab pos="914400" algn="l"/><a:tab pos="1828800" algn="r"/></a:tabLst>}, drawing_xml)
+  ensure
+    File.delete(xlsx_path) if xlsx_path && File.exist?(xlsx_path)
+  end
+
   test "add_shape with text_font alt_lang emits altLang attribute on a:rPr" do
     writer = Xlsxrb::Writer.new
     writer.set_cell("A1", 1)
