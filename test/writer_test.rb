041f2c5de43d523fe91fab94bdbb8f01cb3acf0e
niku <10890+niku@users.noreply.github.com>
2026-04-02 14:20:29 +0000

    Add cat_axis_title_no_fill and val_axis_title_no_fill support
    Writer: already handled via build_title_sp_pr with no_fill hash key
    Reader: parse noFill element within axis title spPr context
    E2E: SDK interoperability tests for both writer and reader

--- test/writer_test.rb
+++ test/writer_test.rb
@@ -6928,6 +6928,25 @@ class WriterTest < Test::Unit::TestCase
     File.delete(xlsx_path) if xlsx_path && File.exist?(xlsx_path)
   end
 
+  test "emits axis title noFill in spPr" do
+    writer = Xlsxrb::Writer.new
+    writer.set_cell("A1", 1)
+    writer.add_chart(type: :bar,
+                     cat_axis_title: { text: "Category", no_fill: true },
+                     val_axis_title: { text: "Value", no_fill: true },
+                     series: [{ val_ref: "Sheet1!$A$1" }])
+    xlsx_tempfile = Tempfile.new(["xlsxrb-axtitlenf", ".xlsx"])
+    xlsx_path = xlsx_tempfile.path
+    xlsx_tempfile.close
+    writer.write(xlsx_path)
+
+    chart_xml = read_xml_from_xlsx(xlsx_path, "xl/charts/chart1.xml")
+    assert_match(%r{<c:catAx>.*<c:title>.*<c:spPr><a:noFill/></c:spPr></c:title>}m, chart_xml)
+    assert_match(%r{<c:valAx>.*<c:title>.*<c:spPr><a:noFill/></c:spPr></c:title>}m, chart_xml)
+  ensure
+    File.delete(xlsx_path) if xlsx_path && File.exist?(xlsx_path)
+  end
+
   test "emits axis fill in spPr" do
     writer = Xlsxrb::Writer.new
     writer.set_cell("A1", 1)
