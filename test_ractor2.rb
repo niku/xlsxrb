f646440f2a36517b2163e80d7a9056a40f0b543d
niku <10890+niku@users.noreply.github.com>
2026-08-06 21:51:06 +0000

    Remove old scratch files

--- test_ractor2.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-require_relative "lib/xlsxrb"
-Ractor.new do
-  puts Xlsxrb::VERSION
-  tracer = OpenTelemetry.tracer_provider.tracer("xlsxrb", Xlsxrb::VERSION)
-  puts tracer
-end.take
