65a4563e44cc2a7ce529e73ff062245d6126b422
niku <10890+niku@users.noreply.github.com>
2026-08-07 00:56:19 +0000

    feat: fast-fail on invalid magic number before parsing zip

---
+++ test/magic_number_test.rb
@@ -0,0 +1,14 @@
+require_relative "test_helper"
+
+class MagicNumberTest < Test::Unit::TestCase
+  test "read raises ArgumentError for invalid magic number" do
+    Tempfile.create(["invalid", ".xlsx"]) do |f|
+      f.write "This is not a zip file"
+      f.close
+      
+      assert_raise(ArgumentError, "Invalid magic number") do
+        Xlsxrb.read(f.path)
+      end
+    end
+  end
+end
