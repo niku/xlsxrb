305e54ad15eaef55b176cf740522b606cea88c45
niku <10890+niku@users.noreply.github.com>
2026-09-03 01:17:16 +0000

    test(zip): add unit tests for ZipGenerator dos_datetime bit calculations, crc32, and directory sizing

--- test/zip_generator_test.rb
+++ test/zip_generator_test.rb
@@ -5,6 +5,7 @@ require "open3"
 require "tempfile"
 
 class ZipGeneratorTest < Test::Unit::TestCase
+  cover Xlsxrb::Ooxml::ZipGenerator
   test "generates a valid zip and preserves entry contents" do
     zip_tempfile = Tempfile.new(["xlsxrb-zip", ".zip"])
     zip_path = zip_tempfile.path
@@ -27,5 +28,42 @@ class ZipGeneratorTest < Test::Unit::TestCase
     assert_equal("world", bar_content)
   ensure
     File.delete(zip_path) if zip_path && File.exist?(zip_path)
+  end
+
+  test "dos_datetime converts Time to MS-DOS packed date and time format" do
+    gen = Xlsxrb::Ooxml::ZipGenerator.new("dummy.zip")
+
+    # 2026-09-03 01:15:30
+    t = Time.new(2026, 9, 3, 1, 15, 30)
+    # Expected: time bytes [0xEF, 0x09], date bytes [0x23, 0x5D]
+    assert_equal([239, 9, 35, 93], gen.send(:dos_datetime, t))
+
+    # Boundary: MS-DOS epoch 1980-01-01 00:00:00
+    t_epoch = Time.new(1980, 1, 1, 0, 0, 0)
+    # date: (0 << 9) | (1 << 5) | 1 = 33 -> [33, 0]
+    # time: 0 -> [0, 0]
+    assert_equal([0, 0, 33, 0], gen.send(:dos_datetime, t_epoch))
+
+    # Odd seconds are truncated to 2-second resolution (31 sec -> 15)
+    t_odd = Time.new(2026, 9, 3, 1, 15, 31)
+    assert_equal([239, 9, 35, 93], gen.send(:dos_datetime, t_odd))
+  end
+
+  test "crc32, le16, le32, and central directory size calculations" do
+    gen = Xlsxrb::Ooxml::ZipGenerator.new("dummy.zip")
+
+    # crc32
+    assert_equal(907_060_870, gen.send(:crc32, "hello"))
+    assert_equal(0, gen.send(:crc32, ""))
+
+    # le16 and le32 little-endian byte arrays
+    assert_equal([0x34, 0x12], gen.send(:le16, 0x1234))
+    assert_equal([0x78, 0x56, 0x34, 0x12], gen.send(:le32, 0x12345678))
+
+    # calculate_central_dir_size (46 bytes fixed header + pathname bytesize)
+    gen.add_entry("a.txt", "content1")
+    gen.add_entry("sub/b.txt", "content2")
+    # (46 + 5) + (46 + 9) = 51 + 55 = 106
+    assert_equal(106, gen.send(:calculate_central_dir_size))
   end
 end
